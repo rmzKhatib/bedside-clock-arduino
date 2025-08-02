000e9257e9954b146f0935151a4fbf18efbd4ab0
rmzKhatib <ramzikhatib07@gmail.com>
2025-08-02 18:16:37 -0400

    Update code in bedside_clock
    - Converted time to 12-hour format
    - Added buzzer alarm at 7:00 AM
    - Colon now blinks every second

--- src/bedside_clock.ino
+++ src/bedside_clock.ino
@@ -2,16 +2,28 @@
 #include <TM1637Display.h>
 #include <RTClib.h>
 
-// TM1637 display pins
-#define CLK 2
-#define DIO 3
+// Pins
+#define CLK 2     // TM1637 Clock
+#define DIO 3     // TM1637 Data
+#define BUZZER 4  // Active Buzzer Pin
+
 TM1637Display display(CLK, DIO);
 
 // RTC
 RTC_DS3231 rtc;
 
+// Alarm Time
+const int alarmHour = 7;
+const int alarmMinute = 0;
+bool alarmTriggered = false; // To avoid buzzing multiple times in one minute
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, LOW);
+
+  display.setBrightness(0x0f); //Max brightness
 
   // Start RTC
   if (!rtc.begin()) {
@@ -21,9 +33,6 @@ void setup() {
 
   // Uncomment this line ONLY the first time to set the time
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-  // Start Display
-  display.setBrightness(0x0f); // Max brightness
   
 }
 
@@ -32,11 +41,38 @@ void loop() {
   DateTime now = rtc.now();
 
   int hour = now.hour();
-  int minute = now.minute();
+  if (hour == 0) {
 
-  // Display time in HH:MM
+    hour = 12;
+
+  } else if (hour > 12) {
+
+    hour -= 12;
+
+  }
+
+  int minute = now.minute();
   int displayTime = hour * 100 + minute;
-  display.showNumberDecEx(displayTime, 0b11100000, true); // 0b11100000 enables colon
+
+  bool colon = now.second() % 2 == 0;
+
+  display.showNumberDecEx(displayTime, colon ? 0b11100000 : 0b00000000, true);
+
+  // Alarm Logic
+  if(now.hour() == alarmHour && now.minute() == alarmMinute && !alarmTriggered) {
+
+    digitalWrite(BUZZER, HIGH);
+    delay(5000); // buzz for 5 seconds
+    digitalWrite(BUZZER, LOW);
+    alarmTriggered = true; // Prevent repeating alarm in same minute
+
+  }
+
+  if (now.minute() != alarmMinute) {
+
+    alarmTriggered = false;
+
+  }
 
   delay(1000);
 
